ba21d0e35627bad50f8cc6afdca34b917463740a
techmannih <manishchaudharyttt@gmail.com>
2024-07-11 23:22:44 +0530

    add vehicle in user vehicle

--- src/components/Vehicle/UserVehicle.js
+++ src/components/Vehicle/UserVehicle.js
@@ -1,14 +1,16 @@
 // src/components/UserVehicle.js
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchVehicleByUserId } from "../../redux/actions/vehicleAction"; // Import your API function for fetching vehicles
+import CreateVehicleModal from './CreateVehicle';
 
 const UserVehicle = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
   const userId = user ? user.id : null; // Assuming you have access to user ID in Redux state
   const userVehicles = useSelector((state) => state.vehicle.userVehicles); // Assuming your reducer stores fetched vehicles
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     if (userId) {
@@ -16,9 +18,23 @@ const UserVehicle = () => {
     }
   }, [dispatch, userId]);
 
+  const handleCreateClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-semibold mb-4">User Vehicles</h1>
+      <h1 className="text-3xl font-bold text-white">Vehicles</h1>
+      <button
+        onClick={handleCreateClick}
+        className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+      >
+        Create Vehicle
+      </button>
       {userVehicles.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
           {userVehicles.map((vehicle) => (
@@ -40,6 +56,7 @@ const UserVehicle = () => {
       ) : (
         <p className="text-gray-600">No user vehicles found for this user.</p>
       )}
+      {isModalOpen && <CreateVehicleModal onClose={handleCloseModal} />}
     </div>
   );
 };
