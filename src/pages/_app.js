63b248de87e2197b931c2627354f979a1b3881c8
techmannih <manishchaudharyttt@gmail.com>
2024-07-11 08:14:41 +0530

    add navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
-// _app.js or main app component
+// pages/_app.js
+import { Provider } from "react-redux";
+import store from "../redux/store"; // Ensure the correct path to the store
+import "./globals.css"; // Ensure your global styles are imported correctly
 import { Toaster } from "react-hot-toast";
+import Navbar from "../components/Navbar/navbar"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Toaster position="top-right" />
+      <Navbar />
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
