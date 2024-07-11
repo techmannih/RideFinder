80bcf485b364d1209484f79802b85f7df244ec57
techmannih <manishchaudharyttt@gmail.com>
2024-07-11 17:03:06 +0530

    fix token expiry time

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,21 @@
-// utils/auth.js
-import jwt_decode from 'jwt-decode';
-import { setUser, clearUser } from '../redux/actions/userAction';
-import { toast } from 'react-hot-toast';
+import { jwtDecode } from "jwt-decode";
+import { setUser, clearUser } from "../redux/actions/userAction";
+import { toast } from "react-hot-toast";
 
 export const validateToken = (dispatch) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
+  console.log("token:", token);
   if (token) {
     try {
-      const decodedToken = jwt_decode(token);
+      const decodedToken = jwtDecode(token);
+
       const currentTime = Date.now() / 1000; // Convert to seconds
+      console.log("decodedToken:", decodedToken);
+      console.log("currentTime:", currentTime);
 
       if (decodedToken.exp < currentTime) {
         // Token has expired, handle logout
-        localStorage.removeItem('token');
+        localStorage.removeItem("token");
         dispatch(clearUser());
         toast.error("Session expired, please login again.");
       } else {
@@ -20,8 +23,8 @@ export const validateToken = (dispatch) => {
         dispatch(setUser(decodedToken.user));
       }
     } catch (error) {
-      console.error('Error decoding token:', error);
-      localStorage.removeItem('token');
+      console.error("Error decoding token:", error);
+      localStorage.removeItem("token");
       dispatch(clearUser());
       toast.error("Session invalid, please login again.");
     }
