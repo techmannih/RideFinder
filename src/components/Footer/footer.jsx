586e90f57f8a19b2ec8b4d512a2033f66e35b37a
techmannih <manishchaudharyttt@gmail.com>
2024-07-13 18:22:56 +0530

    add linkedin on footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import { FaLinkedin } from "react-icons/fa"; // Import LinkedIn icon from react-icons
+import { FaLinkedin } from "react-icons/fa"; // Import LinkedIn icon from react-icons
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -8,18 +8,18 @@ const Footer = () => {
     <footer className="bg-black text-white pt-6 mt-3 max-md:text-sm">
       <div className="container mx-auto text-center py-6">
         <p className="text-lg max-md:text-sm">
-          Ecommerce &copy; {currentYear} All rights reserved.
+          RideFinder &copy; {currentYear} All rights reserved.
         </p>
         <p className="mt-2 flex justify-center items-center">
           Made by Manish Chaudhry
-          {/* <a
+          <a
             href="https://www.linkedin.com/in/techmannih"
             target="_blank"
             rel="noopener noreferrer"
             className="ml-2 text-blue-500"
           >
             <FaLinkedin size={24} />
-          </a> */}
+          </a>
         </p>
       </div>
     </footer>
